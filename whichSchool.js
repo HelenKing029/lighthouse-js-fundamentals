49cf4e161c4a5fbbce539cfb0cf65da2812e8385
HelenKing029 <helenking029@gmail.com>
2016-12-09 04:46:02 +0000

    adding learning task whichSchool

---
+++ whichSchool.js
@@ -0,0 +1,12 @@
+var age = 32;
+
+function whichSchool(age) {
+  // Your code in here ...
+  if (age < 13) {
+    console.log("Elementary School");
+  } else if (age > 13 && < 18) {
+    console.log("Secondary School");
+  } else {
+    console.log("Lighthouse Labs");
+  }
+}
